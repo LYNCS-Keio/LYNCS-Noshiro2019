e39c24dc8768ae37b8ed31fe31b9cea5b9f3e2d6
RyosukeSasaki <rsasaki@keio.jp>
2019-07-05 20:26:13 +0900

    パッケージが生えました

--- raspberry_pi/lib/servo/core.py
+++ raspberry_pi/lib/servo/core.py
@@ -1,19 +1,47 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+"""
+PWM signal: 50 Hz
+dutyCycle       5    -   7.5   -    10
+rotation    backward - nuetral - forward
+"""
 import RPi.GPIO as GPIO
 import time
 
-__all__ = ['servo_pulse'];
+pin1 = 12
+pin2 = 18
 
-GPIO.setmode(GPIO.BCM)
+__all__ = ['servo_pulse']
 
-gp_out = 4
-GPIO.setup(gp_out, GPIO.OUT)
+class servo:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin1, GPIO.OUT)
+        GPIO.setup(pin2, GPIO.OUT)
+        servo1 = GPIO.PWM(pin1, 50)
+        servo2 = GPIO.PWM(pin2, 50)
+        servo1.start(7.5)
+        servo2.start(7.5)
 
-servo = GPIO.PWM(gp_out, 50)
+    def rotate(select,duty):
+        if select == 0:
+            servo1.changeDutyCycle(duty)
+        elif select == 1:
+            servo2.changeDutyCycle(duty)
+        else:
+            pass
 
-def servo_pulse(h):
-    
-    servo.start(h/20000)
-    time.sleep(0.5)
-    servo.stop()
+    def __del__():
+        servo1.stop()
+        servo2.stop()
+        GPIO.cleanup()
 
-GPIO.cleanup()
+if __name__ == "__main__":
+    try:
+        srv = servo()
+        srv.rotate(0, 5)
+        time.sleep(1)
+        srv.rotate(0, 10)
+        time.sleep(1)
+    finally:
+        del srv
