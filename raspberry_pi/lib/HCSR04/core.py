1815e7eea7e94af03f149f97ac50501d4aaed160
RyosukeSasaki <rsasaki@keio.jp>
2019-07-05 21:42:15 +0900

    はえはえのはえ

--- raspberry_pi/lib/HCSR04/core.py
+++ raspberry_pi/lib/HCSR04/core.py
@@ -3,40 +3,44 @@
 import RPi.GPIO as GPIO
 import time
 
-__all__ = ['readData']
+class HCSR04:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+        SOUND_VELOCITY = 34000
+        HC_TRIG = 2
+        HC_ECHO = 3
 
-SOUND_VELOCITY = 34000
-HC_TRIG = 2
-HC_ECHO = 3
+        GPIO.setup(HC_TRIG, GPIO.OUT)
+        GPIO.setup(HC_ECHO, GPIO.IN)
 
-GPIO.setup(HC_TRIG, GPIO.OUT)
-GPIO.setup(HC_ECHO, GPIO.IN)
+    def readData():
+        GPIO.output(HC_TRIG, True)
+        time.sleep(0.000010)
+        GPIO.output(HC_TRIG, False)
 
-def readData():
-    GPIO.output(HC_TRIG, True)
-    time.sleep(0.000010)
-    GPIO.output(HC_TRIG, False)
+        while GPIO.input(HC_ECHO) == GPIO.LOW:
+            S_OFF = time.time()
+        while GPIO.input(HC_ECHO) == GPIO.HIGH:
+            S_ON = time.time()
 
-    while GPIO.input(HC_ECHO) == GPIO.LOW:
-        S_OFF = time.time()
-    while GPIO.input(HC_ECHO) == GPIO.HIGH:
-        S_ON = time.time()
+        D_TIME = S_ON - S_OFF
+        distance = (D_TIME * SOUND_VELOCITY)/2
+        if distance >= 400:
+            distance = 400
+        elif distance <= 20:
+            distance = 20
+        else:
+            distance = distance
+        return distance
 
-    D_TIME = S_ON - S_OFF
-    distance = (D_TIME * SOUND_VELOCITY)/2
-    if distance >= 400:
-        distance = 400
-    elif distance <= 20:
-        distance = 20
-    else:
-        pass
-        #distance = distance
-    return distance
+    def __del__(self):
+        GPIO.cleanup()
 
-if __name__ == '__main__':
-    while True:
-        print(readData())
-        time.sleep(0.2)
+if __name__ == "__main__":
+    try:
+        HCS = HCSR04()
+        print(HCS.readData())
+    finally:
+        del HCS
