d7b78eb44c5ebd3eb6043ffeffb8703fc2a3c789
RyosukeSasaki <rsasaki@keio.jp>
2019-07-05 22:03:15 +0900

    test

--- raspberry_pi/lib/HCSR04/core.py
+++ raspberry_pi/lib/HCSR04/core.py
@@ -3,14 +3,15 @@
 import RPi.GPIO as GPIO
 import time
 
+HC_TRIG = 2
+HC_ECHO = 3
+
 class HCSR04:
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
 
         SOUND_VELOCITY = 34000
-        HC_TRIG = 2
-        HC_ECHO = 3
 
         GPIO.setup(HC_TRIG, GPIO.OUT)
         GPIO.setup(HC_ECHO, GPIO.IN)
