c634289bc433be15e6c9603ff3a6c87adeda5d00
NaokiYano <puni87gou@gmail.com>
2019-07-05 11:39:17 +0900

    ファイルのディレクトリを変えた。あと文法ミスの修正。

--- raspberry_pi/landing.py
+++ raspberry_pi/landing.py
@@ -1,6 +1,6 @@
-from .BME280 import core as BME
-from .HCSR04 import core as HCSR
-from .MPU6050 import mpu6050 as MPU
+from lib import BME280 as BME
+from lib import HCSR04 as HCSR
+from lib import MPU6050 as MPU
 import time
 time_range=5 #加速度が落ち着いている判定判定時間
 break_time=30 #キャリア判定からの経過時間による着陸判定用
@@ -8,6 +8,7 @@ extent=0.02 #MPUの誤差込みの判定にするための変数
 
 #キャリア判定
 now_t = time()
+height = BME.readData()
 
 while height>5: #meter
     height = BME.readData()
@@ -22,7 +23,7 @@ while time()-_time <= time_range :
     a_x,a_y,a_z = MPU.get_accel_data_lsb()
     accel=((a_x-a_y)**2+(a_y-a_z)**2+(a_z-a_x)**2)**0.5
     if 1+extent > accel and accel > 1-extent :
-        if flag=0:
+        if flag == 0:
             flag=1
             _time=time()
     else:
