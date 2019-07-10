92377070f0ae5bcef5d8b14a165ae802ba90016f
RyosukeSasaki <rsasaki@keio.jp>
2019-07-11 01:49:56 +0900

    タイムアウトを追加

--- raspberry_pi/lib/HCSR04/core.py
+++ raspberry_pi/lib/HCSR04/core.py
@@ -5,53 +5,55 @@ import time
 import sys
 """
 -----Usage--------------
-with HCSR04 as hcs:
+with HCSR04(trigger, echo) as hcs:
     print(hcs.readData())
 ------------------------
 """
 
 class HCSR04:
-    def __init__(self, tri, ech, vel):
+    def __init__(self, tri, ech):
         self.trig = tri
         self.echo = ech
-        self.sound_velocity = vel
         
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(self.trig, GPIO.OUT)
         GPIO.setup(self.echo, GPIO.IN)
 
+
     def __enter__(self):
         return self
+
         
-    def readData(self):
+    def readData(self, vel):
+        self.sound_velocity = vel
+        self.outrange = 400 / self.sound_velocity
         GPIO.output(self.trig, True)
         time.sleep(0.000010)
         GPIO.output(self.trig, False)
 
-        while GPIO.input(self.echo) == GPIO.LOW:
-            self.time_1 = time.time()
-        while GPIO.input(self.echo) == GPIO.HIGH:
-            self.delta = time.time() - self.time_1
+        GPIO.wait_for_edge(self.echo, GPIO.RISING, timeout=0.01)
+        self.time_1 = time.time()
+        GPIO.wait_for_edge(self.echo, GPIO.FALLING, timeout=self.outrange)
+        self.delta = time.time() - self.time_1 + 0.0002
 
         self.distance = (self.delta * self.sound_velocity)/2
-        """
-        if self.distance >= 400:
-            self.distance = 400
-        elif self.distance <= 20:
-            self.distance = 20
-        else:
-            pass
-        """
         return self.distance
 
+
     def __exit__(self, exception_type, exception_value, traceback):
+        self.__del__()
+
+
+    def __del__(self):
         GPIO.cleanup(self.trig)
         GPIO.cleanup(self.echo)
+
+
 
 if __name__ == "__main__":
     args = sys.argv
     with HCSR04(int(args[1]), int(args[2]), 34300) as hcs:
         while True:
             hcs.readData()
-            time.sleep(0.05)
+            time.sleep(0.06)
