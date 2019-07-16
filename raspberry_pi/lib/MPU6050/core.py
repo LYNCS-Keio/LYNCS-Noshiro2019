745c22a142febd3936820e28637296edcdb9d024
RyosukeSasaki <rsasaki@keio.jp>
2019-07-16 21:00:41 +0900

    fix

--- raspberry_pi/lib/MPU6050/core.py
+++ raspberry_pi/lib/MPU6050/core.py
@@ -99,6 +99,6 @@ if __name__ == '__main__':
         accel_x, accel_y, accel_z = mpu.get_accel_data_lsb()
         slope_theta, slope_psi, slope_phi = mpu.slope(accel_x, accel_y, accel_z)
 
-        print str(gyro_x) + ',' + str(gyro_y) + ',' + str(gyro_z) + ',' + str(accel_x) + ',' + str(accel_y) + ',' + str(accel_z) + ',' + 
+        print str(gyro_x) + ',' + str(gyro_y) + ',' + str(gyro_z) + ',' + str(accel_x) + ',' + str(accel_y) + ',' + str(accel_z) + ','
         #print str(slope_theta) + ',' + str(slope_psi) + ',' + str(slope_phi)
         time.sleep(0.1)
