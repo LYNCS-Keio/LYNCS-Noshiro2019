8720b5aecf5c583f67cfaefb586ce1dfe324eccb
ujinakada <persodou@gmail.com>
2019-07-20 20:15:35 +0900

    with修正。

--- raspberry_pi/gps_homing.py
+++ raspberry_pi/gps_homing.py
@@ -46,42 +46,41 @@ with servo(pinL) as svL, servo(pinR) as svR:
     svR.rotate(dutyR)
     MPU = MPU6050.MPU6050()
 
-#goalとの距離が10m以下になったら画像での誘導
-while to_goal[0] > cam_dis:
+    #goalとの距離が10m以下になったら画像での誘導
+    while to_goal[0] > cam_dis:
 
-    now = [None, None]
-    now = GPS.lat_long_measurement()
-    if now[0] != None or now[1] != None:
-        convert_now = GPS.convert_lat_long_to_r_theta(pre[0],pre[1],now[1],now[2])
-        rotation_angle = convert_now[1]
-        to_goal =  GPS.convert_lat_long_to_r_theta(now[0],now[1],goal_lat,goal_long)
-        pre = now
+        now = [None, None]
+        now = GPS.lat_long_measurement()
+        if now[0] != None or now[1] != None:
+            convert_now = GPS.convert_lat_long_to_r_theta(pre[0],pre[1],now[1],now[2])
+            rotation_angle = convert_now[1]
+            to_goal =  GPS.convert_lat_long_to_r_theta(now[0],now[1],goal_lat,goal_long)
+            pre = now
 
-    else:
-        #回転
-        pre_gyro = math.radians(MPU.get_gyro_data_lsb()[2]) #degree to radian
-        preT, pre_gyro, now_rotation_angle = cal_rotation_angle(preT, pre_gyro)
-        rotation_angle += now_rotation_angle
+        else:
+            #回転
+            pre_gyro = math.radians(MPU.get_gyro_data_lsb()[2]) #degree to radian
+            preT, pre_gyro, now_rotation_angle = cal_rotation_angle(preT, pre_gyro)
+            rotation_angle += now_rotation_angle
 
-    #dutyLを変える
-    e1 = e
-    e2 = e1
-    e = to_goal[1] - rotation_angle
-    M += Kp * (e-e1) + Ki * e + Kd * ((e-e1) - (e1-e2))
+        #dutyLを変える
+        e1 = e
+        e2 = e1
+        e = to_goal[1] - rotation_angle
+        M += Kp * (e-e1) + Ki * e + Kd * ((e-e1) - (e1-e2))
 
-    if difference < math.pi/2 and difference > -math.pi/2:
-        zenshin = 1
-    else:
-        zenshin = 0
+        if difference < math.pi/2 and difference > -math.pi/2:
+            zenshin = 1
+        else:
+            zenshin = 0
 
-    if M > 1:
-        M = 1
-    if M < -1:
-        M = -1
+        if M > 1:
+            M = 1
+        if M < -1:
+            M = -1
 
-    dutyL = 7.5 + 2.5*((zenshin + M) / 2)
-    dutyR = 7.5 + 2.5*((zenshin - M) / 2)
+        dutyL = 7.5 + 2.5*((zenshin + M) / 2)
+        dutyR = 7.5 + 2.5*((zenshin - M) / 2)
 
-    with servo(pinL) as svL, servo(pinR) as svR:
         svL.rotate(dutyL)
         svR.rotate(dutyR)
