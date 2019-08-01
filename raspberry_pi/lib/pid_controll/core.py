0a3e316447bba31b60088d6ab18ddf949abc870a
Naoki Yano <puni87gou@gmail.com>
2019-08-01 17:46:29 +0900

    Update core.py

--- raspberry_pi/lib/pid_controll/core.py
+++ raspberry_pi/lib/pid_controll/core.py
@@ -5,12 +5,12 @@ __all__=['pid']
 
 class pid(object):
     def __init__(self, Kp, Ki, Kd):
-        self.m, self.e, self.e_prev = 0, 0, 0
+        self.m, self.e, self.e_prev, self.integral= 0, 0, 0, 0
         self.Kp, self.Ki, self.Kd = Kp, Ki, Kd
 
     def update_pid(self, goal, val, dt):
         self.e = goal - val
-        self.integral = self.e * dt
+        self.integral += self.e * dt
         self.m = self.Kp * self.e + self.Ki * self.integral + self.Kd * (self.e - self.e_prev) / dt
         self.e_prev = self.e
         
