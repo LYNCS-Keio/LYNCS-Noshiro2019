1235a357e8a8205a55f228c2e8fbf73dc30728bd
Ryosuke <rsasaki@keio.jp>
2019-08-01 19:11:45 +0900

    warning false

--- raspberry_pi/gps_homing.py
+++ raspberry_pi/gps_homing.py
@@ -7,7 +7,8 @@ import math
 import time
 
 DMUX_pin=[11,9,10] #マルチプレクサの出力指定ピンA,B,C
-DMUX_out=[1,0,0] #出力ピン指定のHIGH,LOWデータ
+DMUX_out = [1, 0, 0]  #出力ピン指定のHIGH,LOWデータ
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DMUX_pin[0], GPIO.OUT)
 GPIO.setup(DMUX_pin[1], GPIO.OUT)
@@ -54,51 +55,54 @@ pre=[None,None]
 while pre[0] is None:
     pre = GPS.lat_long_measurement()
 
-with servo.servo(pinL) as svL, servo.servo(pinR) as svR:
-    svL.rotate(dutyL)
-    svR.rotate(dutyR)
-    MPU = MPU6050.MPU6050(0x68)
-    to_goal , rotation_angle = [1,0] , 0
-    flag = 0
-    #goalとの距離が10m以下になったら画像での誘導
-    while 1:
-        now = [None, None]
-        now = GPS.lat_long_measurement()
-        if now[0] != None and now[1] != None:
-            convert_now = GPS.convert_lat_long_to_r_theta(pre[0],pre[1],now[1],now[2])
-            rotation_angle = convert_now[1]
-            to_goal =  GPS.convert_lat_long_to_r_theta(now[0],now[1],goal_lat,goal_long)
-            pre = now
-            flag = 1
-            if to_goal[0] < cam_dis:
-                svR.rotate(7.5)
-                svL.rotate(7.5)
-                break
-
-        elif flag == 1:
-            #回転
-            pre_gyro = math.radians(MPU.get_gyro_data_lsb()[2]) #degree to radian
-            preT, pre_gyro, now_rotation_angle = cal_rotation_angle(preT, pre_gyro)
-            rotation_angle += now_rotation_angle
-
-        #dutyLを変える
-        e2 = e1
-        e1 = e
-        e = to_goal[1] - rotation_angle
-        M += Kp * (e-e1) + Ki * e + Kd * ((e-e1) - (e1-e2))
-
-        if e < math.pi/2 and e > -math.pi/2:
-            zenshin = 1
-        else:
-            zenshin = 0
-
-        if M > 1:
-            M = 1
-        if M < -1:
-            M = -1
-
-        dutyL = 7.5 + 2.5*((zenshin + M) / 2)
-        dutyR = 7.5 - 2.5*((zenshin - M) / 2)
-
+try:
+    with servo.servo(pinL) as svL, servo.servo(pinR) as svR:
         svL.rotate(dutyL)
         svR.rotate(dutyR)
+        MPU = MPU6050.MPU6050(0x68)
+        to_goal , rotation_angle = [1,0] , 0
+        flag = 0
+        #goalとの距離が10m以下になったら画像での誘導
+        while 1:
+            now = [None, None]
+            now = GPS.lat_long_measurement()
+            if now[0] != None and now[1] != None:
+                convert_now = GPS.convert_lat_long_to_r_theta(pre[0],pre[1],now[1],now[2])
+                rotation_angle = convert_now[1]
+                to_goal =  GPS.convert_lat_long_to_r_theta(now[0],now[1],goal_lat,goal_long)
+                pre = now
+                flag = 1
+                if to_goal[0] < cam_dis:
+                    svR.rotate(7.5)
+                    svL.rotate(7.5)
+                    break
+
+            elif flag == 1:
+                #回転
+                pre_gyro = math.radians(MPU.get_gyro_data_lsb()[2]) #degree to radian
+                preT, pre_gyro, now_rotation_angle = cal_rotation_angle(preT, pre_gyro)
+                rotation_angle += now_rotation_angle
+
+            #dutyLを変える
+            e2 = e1
+            e1 = e
+            e = to_goal[1] - rotation_angle
+            M += Kp * (e-e1) + Ki * e + Kd * ((e-e1) - (e1-e2))
+
+            if e < math.pi/2 and e > -math.pi/2:
+                zenshin = 1
+            else:
+                zenshin = 0
+
+            if M > 1:
+                M = 1
+            if M < -1:
+                M = -1
+
+            dutyL = 7.5 + 2.5*((zenshin + M) / 2)
+            dutyR = 7.5 - 2.5*((zenshin - M) / 2)
+
+            svL.rotate(dutyL)
+            svR.rotate(dutyR)
+finally:
+    GPIO.cleanup()
