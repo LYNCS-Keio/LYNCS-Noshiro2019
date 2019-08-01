4c8f6620132f1ae4ba7ad0a6cb8915c1beba2724
ujinakada <persodou@gmail.com>
2019-08-02 01:05:21 +0900

    補正値修正

--- raspberry_pi/MPUtest.py
+++ raspberry_pi/MPUtest.py
@@ -4,7 +4,7 @@ rotation_angle = 0
 def cal_rotation_angle(preT,p_g):
     nowT = time.time()
     now_gyro = MPU.get_gyro_data_lsb()[2]
-    now_rotation_angle = (now_gyro + p_g) * (nowT - preT) / 2 - 0.775
+    now_rotation_angle = (now_gyro + p_g) * (nowT - preT) / 2 - 0.41
     print("変化率:",now_rotation_angle/(nowT - preT))
     return [nowT, now_gyro, now_rotation_angle]
 
