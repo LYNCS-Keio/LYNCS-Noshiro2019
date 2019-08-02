7ee1da3ccc387efaca747debcce96f4131a66314
Ryosuke <rsasaki@keio.jp>
2019-08-02 19:03:28 +0900

    BME消し

--- raspberry_pi/landing.py
+++ raspberry_pi/landing.py
@@ -53,7 +53,7 @@ with servo.servo(pinPWM) as sv: #パラ機構ロック
 
     while 1:
         with HCSR04.HCSR04(trigger,echo) as hcs:
-            height = hcs.readData(vel)
+            height = hcs.readData(34300)
             print (height)
             if (time.time()-now_t > break_time) or ((50 <= height) and (height <= 200)):
                 break
