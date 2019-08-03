3b4cfd8bb58004122aaba4cf07f665e0c7979833
Ryosuke <rsasaki@keio.jp>
2019-08-03 10:35:36 +0900

    gain調整

--- raspberry_pi/go_straight.py
+++ raspberry_pi/go_straight.py
@@ -20,7 +20,7 @@ svL.start(7.5)
 svR.start(7.5)
 
 mpu = MPU6050.MPU6050(0x68)
-p = pid_controll.pid(0.4, 2, 0.008)
+p = pid_controll.pid(0.004, 0.02365, 0.0002436)
 #p = pid_controll.pid(4.8, 23.65, 0.2436)
 pt = time.time()
 
