d58d65ae0605805274e61a8f2a5467368f4ad1e9
Ryosuke <rsasaki@keio.jp>
2019-08-07 17:00:08 +0900

    完成(仮)

--- raspberry_pi/tv_homing.py
+++ raspberry_pi/tv_homing.py
@@ -3,7 +3,7 @@ from lib import camera
 from lib import capture
 from lib import MPU6050
 from lib import pid_controll
-import RPi.GPIO as gpio
+import RPi.gpio as gpio
 import math
 import time
 import threading
@@ -22,13 +22,13 @@ rotation_lock = threading.Lock()
 
 DMUX_pin = [11, 9, 10]  # マルチプレクサの出力指定ピンA,B,C
 DMUX_out = [0, 0, 0]  # 出力ピン指定のHIGH,LOWデータ
-GPIO.setup(DMUX_pin[0], GPIO.OUT)
-GPIO.setup(DMUX_pin[1], GPIO.OUT)
-GPIO.setup(DMUX_pin[2], GPIO.OUT)
+gpio.setup(DMUX_pin[0], gpio.OUT)
+gpio.setup(DMUX_pin[1], gpio.OUT)
+gpio.setup(DMUX_pin[2], gpio.OUT)
 
-GPIO.output(DMUX_pin[0], DMUX_out[0])
-GPIO.output(DMUX_pin[1], DMUX_out[1])
-GPIO.output(DMUX_pin[2], DMUX_out[2])
+gpio.output(DMUX_pin[0], DMUX_out[0])
+gpio.output(DMUX_pin[1], DMUX_out[1])
+gpio.output(DMUX_pin[2], DMUX_out[2])
 
 p = pid_controll.pid(0.004, 0.03, 0.0002436)
 
