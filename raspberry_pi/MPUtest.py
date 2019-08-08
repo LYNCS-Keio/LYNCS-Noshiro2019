fc415cc44ed3cf201ecb63988a2e22080c8d0928
ujinakada <persodou@gmail.com>
2019-08-08 20:54:51 +0900

    drift

--- raspberry_pi/MPUtest.py
+++ raspberry_pi/MPUtest.py
@@ -1,9 +1,11 @@
 from lib import MPU6050
 import time
 rotation_angle = 0
+drift = 1.092913
+
 def cal_rotation_angle(preT,p_g):
     nowT = time.time()
-    now_gyro = MPU.get_gyro_data_lsb()[2] - 0.825
+    now_gyro = MPU.get_gyro_data_lsb()[2] + drift
     now_rotation_angle = (now_gyro + p_g) * (nowT - preT) / 2
     print("変化率:",now_rotation_angle/(nowT - preT))
     return [nowT, now_gyro, now_rotation_angle]
