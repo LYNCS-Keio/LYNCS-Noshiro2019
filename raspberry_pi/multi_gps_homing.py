5891cf7d49227cb44594ff26ad70394ae1668923
ujinakada <persodou@gmail.com>
2019-08-08 13:46:17 +0900

    print

--- raspberry_pi/multi_gps_homing.py
+++ raspberry_pi/multi_gps_homing.py
@@ -67,11 +67,10 @@ def gps_get():
             print(to_goal[0])
             count += 1
             if count == 90:
-                print("count!!!")
                 lock.acquire()
                 to_goal[1] = -math.degrees(GPS.convert_lat_long_to_r_theta(now[0],now[1],goal_lat,goal_long)[1])
                 rotation = -math.degrees(GPS.convert_lat_long_to_r_theta(pre[0], pre[1], now[0], now[1])[1])
-                print(now)
+                print("count!!!", now, GPS.convert_lat_long_to_r_theta(pre[0], pre[1], now[0], now[1])[0])
                 pre = now
                 lock.release()
                 #count = 0
