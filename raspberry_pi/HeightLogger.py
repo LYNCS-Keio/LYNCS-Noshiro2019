7b30b968f74838f366fb34f95f373eea5c572194
ujinakada <persodou@gmail.com>
2019-08-09 22:32:09 +0900

    パラ固定を追加

--- raspberry_pi/HeightLogger.py
+++ raspberry_pi/HeightLogger.py
@@ -3,15 +3,57 @@ from lib import BME280 as BME
 import time
 import csv
 import os
-
+import RPi.GPIO as GPIO
 current_dir = os.path.dirname(os.path.abspath(__file__))
 #print(current_dir)
+
+class servo:
+    def __init__(self, pin):
+        self.pin = pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.srv = GPIO.PWM(self.pin, 50)
+        self.srv.start(7.5)
+
+    def __enter__(self):
+        return self
+
+    def rotate(self, duty):
+        self.srv.ChangeDutyCycle(duty)
+
+    def stop(self):
+        self.srv.stop()
+
+    def __del__(self):
+        self.srv.stop()
+        GPIO.cleanup(self.pin)
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        pass
+
 
 index = 0
 filename = 'hlog' + '%04d' % index
 while os.path.isfile(current_dir + '/' + filename + '.csv') == True:
     index += 1
     filename = 'hlog' + '%04d' % index
+
+DMUX_pin=[11,9,10]
+DMUX_out = [1, 0, 0]
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DMUX_pin[0], GPIO.OUT)
+GPIO.setup(DMUX_pin[1], GPIO.OUT)
+GPIO.setup(DMUX_pin[2], GPIO.OUT)
+
+GPIO.output(DMUX_pin[0], DMUX_out[0])
+GPIO.output(DMUX_pin[1], DMUX_out[1])
+GPIO.output(DMUX_pin[2], DMUX_out[2])
+
+with servo(12) as svP:
+    svP.rotate(7.6)
+    time.sleep(0.1)
+    svP.stop()
 
 with open(current_dir + '/' + filename + '.csv', 'w') as c:
     f = csv.writer(c, lineterminator='\n')
