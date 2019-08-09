5343704dfcc96571bf3810425f89f1b7d006dd3a
ujinakada <persodou@gmail.com>
2019-08-10 08:58:29 +0900

    test

--- raspberry_pi/test.py
+++ raspberry_pi/test.py
@@ -1,12 +1,21 @@
 import time
-from lib import servo
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
+pinDMUX=[11,9,10] #マルチプレクサの出力指定ピンA,B,C
+DMUX_out=[1,0,0] #出力ピン指定のHIGH,LOWデータ
+pinPWM=12 #マルチプレクサ側PWMのピン
+for pin in range(0,2):
+    GPIO.setup(pinDMUX[pin],GPIO.OUT)
+    GPIO.output(pinDMUX[pin],DMUX_out[pin]) #分離サーボの出力指定
+sv = GPIO.PWM(pinPWM, 50)
+sv.start(7.1)
+time.sleep(0.5)
+for x in renge(1,10):
+    y = 7.1 - x*0.05
+    sv.ChangeDutyCycle(y)
+    time.sleep(0.005)
 
-with servo.servo(18) as sv:
-    while True:
-        sv.rotate(7.5)
-        time.sleep(1)
-        sv.rotate(9)
-        time.sleep(1)
+sv.stop()
+time.sleep(1)
+GPIO.cleanup()
