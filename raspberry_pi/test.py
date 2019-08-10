8854ca1528a6dd45439bace72b94da8130aa6719
ujinakada <persodou@gmail.com>
2019-08-10 09:03:28 +0900

    test

--- raspberry_pi/test.py
+++ raspberry_pi/test.py
@@ -12,7 +12,7 @@ GPIO.setup(12,GPIO.OUT)
 sv = GPIO.PWM(pinPWM, 50)
 sv.start(7.1)
 time.sleep(0.5)
-for x in renge(1,10):
+for x in range(1,10):
     y = 7.1 - x*0.05
     sv.ChangeDutyCycle(y)
     time.sleep(0.005)
