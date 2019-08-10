2e9ea5561360fa0da02e04c1e76c1632bd90c392
Ryosuke <rsasaki@keio.jp>
2019-08-10 22:31:27 +0900

    連射テスト

---
+++ raspberry_pi/burst_test.py
@@ -0,0 +1,18 @@
+from lib import camera 
+from lib import capture
+import time
+
+try:
+    cap = capture.capture()
+    cam = camera.CamAnalysis()
+    time.sleep(2)
+    while True:
+        stream = cap.cap()
+        cam.morphology_extract(stream)
+        coord = cam.contour_find()
+        row = [time.time()]
+        row.extend(coord)
+        print(row)
+
+finally:
+    del cap
