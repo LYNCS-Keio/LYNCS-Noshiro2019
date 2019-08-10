5b3c1c8685249a0fe0f57d6c9ef08b19c8c89369
RyosukeSasaki <rsasaki@keio.jp>
2019-08-10 16:16:30 +0900

    goal_lat_long

--- raspberry_pi/multi_gps_homing.py
+++ raspberry_pi/multi_gps_homing.py
@@ -38,7 +38,7 @@ for pin in range(0, 2):
 p = pid_controll.pid(0.004, 0.03, 0.0004)
 
 #goalの座標
-goal_lat, goal_long = 35.554506, 139.656850 #グラウンド
+goal_lat, goal_long = 35.554532, 139.6564 #グラウンド
 #goal_lat, goal_long = 35.5550, 139.6555 #自販機横
 
 drift = -1.032555
