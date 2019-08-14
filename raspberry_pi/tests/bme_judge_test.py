36d945fba761995e58643d2f5eea14cfb240cb50
NaokiYano <puni87gou@gmail.com>
2019-08-14 23:00:09 +0900

    update BME_Judge_Test

--- raspberry_pi/tests/bme_judge_test.py
+++ raspberry_pi/tests/bme_judge_test.py
@@ -6,19 +6,18 @@ class TestBMEJudge(unittest.TestCase):
     def test_in_case_not_at_the_top(self):
         bme_judge = RoverFuncs.BME_Judge()
         reached_flag = False
-        for i in 10:
-            reached_flag = bme_judge.is_reached_the_top(RoverFuncs._height_high - 1)
+        for i in range(bme_judge.limit_bme()):
+            reached_flag = bme_judge.is_reached_top(bme_judge.height_high() - 1)
         self.assertTrue(not reached_flag)
     
     def test_in_case_at_the_top(self):
         bme_judge = RoverFuncs.BME_Judge()
         reached_flag = False
-        for i in 10:
-            reached_flag = bme_judge.is_reached_the_top(RoverFuncs._height_high - 1)
-        for i in 10:
-            reached_flag = bme_judge.is_reached_the_top(RoverFuncs._height_high)
-        self.assertTrue(not reached_flag)
-
+        for i in range(bme_judge.limit_bme()):
+            reached_flag = bme_judge.is_reached_top(bme_judge.height_high() - 1)
+        for i in range(bme_judge.limit_bme()):
+            reached_flag = bme_judge.is_reached_top(bme_judge.height_high())
+        self.assertTrue(reached_flag)
 
 if __name__ == "__main__":
     unittest.main()
